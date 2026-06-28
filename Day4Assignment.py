3580efb8ebce9a24fdf02012e29080d2566f931a
ACL-Krishna <krishna.goswami@acldigital.com>
2026-06-28 12:30:12 +0530

    Changed to python file

--- Day4Assignment.py
+++ Day4Assignment.py
@@ -2,7 +2,6 @@ import pytest
 from playwright.async_api import async_playwright
 from playwright.sync_api import sync_playwright, expect
 
-"""
 # ------Open Google, verify title using pytest ------
 def test_validate_google_login(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -11,9 +10,8 @@ def test_validate_google_login(playwright):
     page.goto("https://google.com")
     print("Title of the page is : ", page.title())
     assert page.title() == "Google" , f"Expected 'Google', got '{page.title()}'"
-"""
 
-"""
+
 # -------Open saucedemo.com, locate username/password/login, print placeholder------
 def test_saucedemo(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -28,10 +26,8 @@ def test_saucedemo(playwright):
     page.fill('input[id=user-name]', 'standard_user') # locating username
     page.fill('input[id="password"]', 'secret_sauce') # locating password
     page.click('input[id="login-button"]') # locating login
-"""
 
 
-"""
 # ----------Perform login, validate success & error scenarios---------
 def test_login(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -46,9 +42,8 @@ def test_login(playwright):
     assert page.title() == "Swag Labs", f"Expected 'Swag Labs', got '{page.title()}'"
     #-------error scenarios-------
     #assert page.title() == "Swag Lab", f"Expected 'Swag Labs', got '{page.title()}'"
-"""
 
-"""
+
 # ---------Verify title, inventory visibility, product count ≥ 6 -----
 def test_saucedemo(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -59,26 +54,17 @@ def test_saucedemo(playwright):
     page.fill('input[id=user-name]', 'standard_user')
     page.fill('input[id="password"]', 'secret_sauce')
     page.click('input[id="login-button"]')
-
     #verify title
     assert page.title() == "Swag Labs", f"Expected 'Swag Labs', got '{page.title()}'"
-
     # verify inventory is visible
     assert page.locator(".inventory_item").count() > 0
     products = page.locator(".inventory_item")
-
     #verify product count >=6
     total_count = products.count()
     print("Total products on page are :", total_count)
-    # if total_count >= 6:
-    #     assert True
-    # else:
-    #     assert False
-
     assert total_count >= 6 , f"Expected at least 6 products, found {total_count}"
-"""
 
-"""
+
 #  -------Sort products, add items, validate cart of saucedemo.com in python with playwright----
 def test_saucedemo(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -89,13 +75,11 @@ def test_saucedemo(playwright):
     page.fill('input[id=user-name]', 'standard_user')
     page.fill('input[id="password"]', 'secret_sauce')
     page.click('input[id="login-button"]')
-
     #-----------Add items---------------
     add_items = page.locator(".inventory_item_name").all_text_contents()
     add_items.append("laptop")
     add_items.append("mobile")
     add_items.append("keyboard")
-    
     #-----------------sort products--------------
     print("---------before sort------------")
     print(add_items)
@@ -103,9 +87,7 @@ def test_saucedemo(playwright):
     print()
     print("---------after sort------------")
     print(add_items)
-
     #-------------validate cart------------
-
     page.locator("#add-to-cart-sauce-labs-backpack").click()
     page.locator("#add-to-cart-sauce-labs-bike-light").click()
     page.locator(".shopping_cart_link").click()
@@ -113,9 +95,7 @@ def test_saucedemo(playwright):
     print()
     print("Total item added to cart are : ", cart)
     assert cart == 2 , f" Expected 2 items in cart, but found {cart}"
- """
 
-"""
 # --------Checkout flow, validate confirmation------
 def test_saucedemo(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -143,4 +123,4 @@ def test_saucedemo(playwright):
     assert confirmation_text == "Thank you for your order!"
     print("order placed successfully")
 
-"""
+
