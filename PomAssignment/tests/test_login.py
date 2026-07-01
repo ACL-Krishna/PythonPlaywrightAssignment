dbf11d9ac31f04bfabaf3d8ce9b198bec6f05aa7
Krishna.G <krishna.goswami@acldigital.com>
2026-07-01 13:06:33 +0530

    Added Github Actions

--- PomAssignment/tests/test_login.py
+++
@@ -1,9 +0,0 @@
-
-from pages.login_page import LoginPage
-def test_saucedemo(page_setup):
-    login_page = LoginPage(page_setup)
-    login_page.open_url()
-    page_setup.locator('#user-name').wait_for(state="visible")
-    page_setup.screenshot(path="screenshots/screenshot.png") # taking screenshot
-    login_page.login('standard_user','secret_sauce')
-    print("----------login successful---------------")
